345d95e321239b350ff9f88289cc61cfe226f9c0
Ruman Syeda <usyeda1@my.centennialcollege.ca>
2025-03-22 18:59:18 -0400

    Basic And Advanced Search Functionality

---
+++ backend/controllers/searchController.js
@@ -0,0 +1,33 @@
+import asyncHandler from "express-async-handler"
+import User from "../models/userModel.js"
+import Event from "../models/eventModel.js"
+
+
+// @desc    Search users, events
+// @route   GET /api/search
+// @access  Private
+const search = asyncHandler(async (req, res) => {
+  const { query, type } = req.query
+  const searchQuery = { $regex: query, $options: "i" }
+
+  const results = {}
+
+  if (!type || type === "users") {
+    const users = await User.find({
+      $or: [{ name: searchQuery }, { email: searchQuery }],
+    }).select("name email profilePicture role")
+    results.users = users
+  }
+
+  if (!type || type === "events") {
+    const events = await Event.find({
+      $or: [{ title: searchQuery }, { description: searchQuery }, { location: searchQuery }, { category: searchQuery }],
+    }).select("title description date location category image")
+    results.events = events
+  }
+
+  res.json(results)
+})
+
+export { search }
+
