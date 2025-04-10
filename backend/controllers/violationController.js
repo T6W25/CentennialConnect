02167f71458c2eb8f21e669d23e640b72a00f669
prakashdevkot <126669724+prakashdevkot@users.noreply.github.com>
2025-04-10 18:05:40 -0400

    api end points user violations

--- backend/controllers/violationController.js
+++ backend/controllers/violationController.js
@@ -24,7 +24,7 @@ const resolveViolation = asyncHandler(async (req, res) => {
   const violation = await Violation.findById(req.params.id);
 
   if (!violation) {
-    res.status(404);
+    res.status(404).json({ message: 'Violation not found' });
     throw new Error('Violation not found');
   }
 
